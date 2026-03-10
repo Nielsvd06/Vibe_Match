af31f3d7a0a03c1b770ad124955b621841f0e3a6
Helena Wali <helicatsventures@gmail.com>
2026-03-10 17:46:40 +0100

    Messages model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_10_152030) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_162519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_152030) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_current_vibes_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.bigint "current_vibe_id", null: false
+    t.string "role"
+    t.datetime "updated_at", null: false
+    t.index ["current_vibe_id"], name: "index_messages_on_current_vibe_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +52,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_152030) do
   end
 
   add_foreign_key "current_vibes", "users"
+  add_foreign_key "messages", "current_vibes"
 end
