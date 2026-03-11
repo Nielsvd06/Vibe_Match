0eed36888ac94db70c31f3f102765098bbda0b29
Helena Wali <helicatsventures@gmail.com>
2026-03-11 11:19:27 +0100

    current-vibes-controller

--- app/controllers/current_vibes_controller.rb
+++ app/controllers/current_vibes_controller.rb
@@ -8,3 +8,10 @@ class CurrentVibesController < ApplicationController
   def update
   end
 end
+
+# current_vibe create action
+# 1 - generate a blank current_vibe with titled as ‘untitled’ for example
+# 2 - if @current_vibe.save statement
+# 3 - make an LLM request passing through a system prompt to generate the first assistant message on the page.
+#   (eg - let’s get started, in order to tailor your ideas we need to ask you some questions, firstly how much time do you have available?) and make a message in the back end ie: Message.create(content: response.content, role: ‘assistant’, current_vibe: @current_vibe)
+# 4 - redirect to current_vibe show page
